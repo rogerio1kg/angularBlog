39a55f3ad05cc29e4eeab2962355959f778ccdf1
rogerio1kg <rogeriolevydesousa@gmail.com>
2023-12-06 01:45:38 -0400

    inicial

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": '1',
+        "photo": '',
+        "title": "",
+        "description": ''
+    },
+    {
+        "id": '2',
+        "photo": '',
+        "title": "",
+        "description": ''
+    },
+    {
+        "id": '3',
+        "photo": '',
+        "title": "",
+        "description": ''
+    },
+    {
+        "id": '4',
+        "photo": '',
+        "title": "",
+        "description": ''
+    }
+]
